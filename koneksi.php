b1715786fb39893aa3606216b0424ad9b54ca833
podenaru <acunmanggala52@gmail.com>
2025-01-18 00:59:08 +0700

    Deskripsi perubahan

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","laundry");
+$koneksi = mysqli_connect("sql12.freesqldatabase.com","sql12758226","Jud8S842dM","sql12758226","3306");
 if (mysqli_connect_errno()){
 echo "Koneksi database gagal : " . mysqli_connect_error(); 
 }
