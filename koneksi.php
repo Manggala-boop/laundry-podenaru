b2a9afe727f615d4ae17eed44d31cde7f356c792
Manggala-boop <manggala@students.amikom.ac.id>
2025-12-29 05:52:50 -0500

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,30 @@
 <?php
 $host = "db-laundry.mysql.database.azure.com";
-$user = "sqladmin";
+$user = "sqladmin@db-laundry"; // WAJIB pakai @servername
 $pass = "@Manggala77";
 $db   = "laundry_podenaru";
+$port = 3306;
 
-$koneksi = mysqli_connect($host, $user, $pass, $db);
+$koneksi = mysqli_init();
 
-if (mysqli_connect_errno()) {
+/*
+ ssl-mode = require
+ artinya: pakai SSL, tapi tidak perlu cert file
+*/
+mysqli_options($koneksi, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
+
+$connected = mysqli_real_connect(
+    $koneksi,
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$connected) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
 ?>
