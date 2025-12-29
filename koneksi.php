f05a2c5933d88ca7577171d2130862f573f6a940
Manggala-boop <manggala@students.amikom.ac.id>
2025-12-29 05:37:49 -0500

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,12 @@
-<?php 
-$koneksi = mysqli_connect(
-    "db-laundry.mysql.database.azure.com",
-    "sqladmin",
-    "@Manggala77",
-    "laundry_podenaru"
-);
-?> 
+<?php
+$host = "db-laundry.mysql.database.azure.com";
+$user = "sqladmin@db-laundry";
+$pass = "@Manggala77";
+$db   = "laundry_podenaru";
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (mysqli_connect_errno()) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
+?>
